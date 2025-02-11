2f321d81da7b2cdd72b52a9fda7acd3868ecb0a7
Charlie Ward <59178341+Charlie-Ward@users.noreply.github.com>
2025-02-11 14:54:23 +0000

    Removed search icon from desktop navbar only leaving the bar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -32,12 +32,6 @@ const Navbar = (props: Props) => {
 
                 {/* Desktop Navigation */}
                 <div className='hidden md:flex items-center gap-4'>
-                    <Button variant="outline" size="icon">
-                        <svg xmlns="http://www.w3.org/2000/svg" className="h-[1.2rem] w-[1.2rem]" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
-                        </svg>
-                    </Button>
-
                     <DropdownMenu>
                         <DropdownMenuTrigger asChild>
                             <Button variant="outline" size="icon">
