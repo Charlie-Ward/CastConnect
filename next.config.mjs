3298a0f45ae062ccbf95f85e4477433497b1a392
Charlie Ward <59178341+Charlie-Ward@users.noreply.github.com>
2025-02-13 16:39:46 +0000

    WORK

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-<<<<<<< HEAD
-=======
-  distDir: 'build',
->>>>>>> a51347ee615dd2bc6e2926f01ed916da073f67f3
   experimental: {
     serverActions: true,
   },
