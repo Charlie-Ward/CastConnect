d7ee516bd3a84134db6a9ddca8ac240b898b863b
Charlie Ward <59178341+Charlie-Ward@users.noreply.github.com>
2025-03-30 16:14:29 +0100

    Add comments to MenuOptions component for clarity on button creation and rendering logic

--- src/components/LeftSide/MenuOptions.tsx
+++ src/components/LeftSide/MenuOptions.tsx
@@ -11,6 +11,7 @@ type MenuItemProps = {
   href: string
 }
 
+// Button creation function
 const MenuItem = ({icon, label, href}: MenuItemProps) => {
     return(
         <Link href={href} className='flex items-center space-x-6 py-2 hover:text-pink-500 cursor-pointer opacity-80 hover:opacity-100'>
@@ -28,16 +29,17 @@ const MenuOptions = (props: Props) => {
         {icon: <LayoutGrid />, label: 'My Casting Calls', href: '/my-casting-calls'},
     ]
 
-  return (
-    <div className='lg:w-1/4 md:1/5 p-5 max-md:hidden my-5'>
-        <div className='flex flex-col'>
-            {MenuItems.map((item, index) => (
-                <MenuItem key={index} icon={item.icon} label={item.label} href={item.href} />
-            ))}
-            <Separator className='my-4' />
+    //The return of the page. Runs over each entry of the array and calls the creation function with the data of each entry in the array.
+    return (
+        <div className='lg:w-1/4 md:1/5 p-5 max-md:hidden my-5'>
+            <div className='flex flex-col'>
+                {MenuItems.map((item, index) => (
+                    <MenuItem key={index} icon={item.icon} label={item.label} href={item.href} />
+                ))}
+                <Separator className='my-4' />
+            </div>
         </div>
-    </div>
-  )
+    )
 }
 
 export default MenuOptions
